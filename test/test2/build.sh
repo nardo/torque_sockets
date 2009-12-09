9e71a3af2996fd6a2c658107d58c3b085510dd86
Alex Scarborough <alex@gameclay.com>
2009-12-08 21:38:26 -0800

    Don't need test2 build.sh anymore, hello SCons

--- test/test2/build.sh
+++
@@ -1,3 +0,0 @@
-gcc-4.2 -o client client_main.cpp -g -O0 -I../../lib/libtommath -I../../platform_library -I../../lib/libtomcrypt/src/headers -lstdc++ -ltomcrypt -ltommath -L../../lib/libtommath -L../../lib/libtomcrypt
-
-gcc-4.2 -o server server_main.cpp -g -O0 -I../../lib/libtommath -I../../platform_library -I../../lib/libtomcrypt/src/headers -lstdc++ -ltomcrypt -ltommath -L../../lib/libtommath -L../../lib/libtomcrypt
