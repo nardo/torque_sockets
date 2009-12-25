744473ec9c82df223245708b906cd585df38a0e2
Mark Frohnmayer <mark.frohnmayer@gmail.com>
2009-12-24 17:55:33 -0800

    first pass at the arranged connection API.

--- platform_library/torque_sockets.h
+++ platform_library/torque_sockets.h
@@ -16,11 +16,13 @@ extern "C" {
 struct _torque_opaque_socket_t;
 typedef struct _torque_opaque_socket_t* torque_socket;
 typedef uint32 torque_connection;
+static const torque_connection invalid_torque_connection = 0;
 struct torque_socket_event;
 	
 enum connection_constants {
 	torque_max_datagram_size = 1516,
-	torque_max_status_datagram_size = 512,	
+	torque_max_status_datagram_size = 512,
+	torque_max_public_key_size = 512,
 };
 	
 torque_socket torque_socket_create(const SOCKADDR *); ///< Create a torque socket and bind it to the specified socket address interface.
@@ -28,18 +30,17 @@ void torque_socket_destroy(torque_socket); ///< Close the specified socket; any 
 void torque_socket_seed_entropy(torque_socket, uint8 entropy[32]); ///< Seed random entropy data for this socket (used in the generation of cryptographic keys).
 void torque_socket_read_entropy(torque_socket, uint8 entropy[32]); ///< Read the current entropy state from this socket.
 void torque_socket_set_private_key(torque_socket, uint32 key_data_size, uint8 *the_key); ///< Sets the private/public key pair to be used for this connection; these are formatted as libtomcrypt keys, and currently only ECC is supported.
-void torque_socket_set_challenge_response_data(torque_socket, uint32 challenge_response_data_size, uint8 challenge_response_data[torque_max__status_datagram_size]); ///< Sets the data to be sent back upon challenge request along with the client puzzle and public key.  challenge_response_data_size must be <= torque_max_status_datagram_size
+void torque_socket_set_challenge_response_data(torque_socket, uint32 challenge_response_data_size, uint8 challenge_response_data[torque_max_status_datagram_size]); ///< Sets the data to be sent back upon challenge request along with the client puzzle and public key.  challenge_response_data_size must be <= torque_max_status_datagram_size
 void torque_socket_allow_incoming_connections(torque_socket, bool allowed); ///< Sets whether or not this connection accepts incoming connections; if not, all incoming connection challenges and requests will be silently ignored.
 bool torque_socket_does_allow_incoming_connections(torque_socket); ///< Returns true if this socket allows incoming connections.
+void torque_socket_allow_arranged_connections(torque_socket, bool allowed); ///< if true, this socket will arrange connections between hosts connected to this socket.
+bool torque_socket_does_allow_arranged_connections(torque_socket); ///< Returns true if this socket will arrange connections between hosts connected to this socket.	
 torque_socket_event *torque_socket_get_next_event(torque_socket); ///< Gets the next event on this socket; returns NULL if there are no events to be read.
-
-torque_connection torque_socket_connect(torque_socket, const SOCKADDR *remote_host, uint32 connect_data_size, uint8 connect_data[torque_max__status_datagram_size]); ///< open a connection to the remote host
-
-void torque_connection_accept(torque_connection, uint32 connect_accept_data_size, uint8 connect_accept_data[torque_max__status_datagram_size]); ///< accept an incoming connection request.
-
+torque_connection torque_socket_connect(torque_socket, const SOCKADDR *remote_host, uint32 connect_data_size, uint8 connect_data[torque_max_status_datagram_size]); ///< open a connection to the remote host
+torque_connection torque_connection_connect_arranged(torque_connection middle_man, uint64 remote_client_identity, uint32 connect_data_size, uint8 connect_data[torque_max_status_datagram_size]); ///< Connect to a client connected to the host at middle_man.
+void torque_connection_accept(torque_connection, uint32 connect_accept_data_size, uint8 connect_accept_data[torque_max_status_datagram_size]); ///< accept an incoming connection request.
 void torque_connection_reject(torque_connection, uint32 reject_data_size, uint8 reject_data[torque_max__status_datagram_size]); ///< reject an incoming connection request.
-
-void torque_connection_disconnect(torque_connection, uint32 disconnect_data_size, uint8 disconnect_data[torque_max__status_datagram_size]); ///< Close an open connection. 
+void torque_connection_disconnect(torque_connection, uint32 disconnect_data_size, uint8 disconnect_data[torque_max_status_datagram_size]); ///< Close an open connection. 
 send_to_result torque_connection_send_to(torque_connection, uint32 datagram_size, uint8 buffer[torque_max_datagram_size], uint32 *sequence_number); ///< Send a datagram packet to the remote host on the other side of the connection.  Returns the sequence number of the packet sent.
 
 struct torque_socket_event
@@ -48,6 +49,7 @@ struct torque_socket_event
 	{
 		torque_connection_challenge_response_event_type,
 		torque_connection_requested_event_type,
+		torque_connection_arranged_connection_request_event_type,
 		torque_connection_accepted_event_type,
 		torque_connection_rejected_event_type,
 		torque_connection_timed_out_event_type,
@@ -60,9 +62,19 @@ struct torque_socket_event
 	uint32 event_type;
 };
 	
+struct torque_connection_challenge_response_event : torque_socket_event
+{
+	torque_connection the_connection;
+	uint32 public_key_size,
+	uint8 public_key[torque_max_public_key_size];
+	uint32 challenge_response_data_size;
+	uint8 challenge_response_data[torque_max_status_datagram_size]; 
+};
+	
 struct torque_connection_accepted_event : torque_socket_event
 {
 	torque_connection the_connection;
+	uint64 client_identity;
 	uint32 connection_accept_data_size;
 	uint8 connection_accept_data[torque_max_status_datagram_size];
 };
@@ -82,6 +94,15 @@ struct torque_connection_requested_event : torque_socket_event
 	uint8 connection_request_data[torque_max_status_datagram_size];
 };
 
+struct torque_connection_arranged_connection_request_event : torque_socket_event
+{
+	uint64 source_client_identity;
+	torque_connection the_connection;
+	torque_connection arranger_connection;
+	uint32 connection_request_data_size;
+	uint8 connection_request_data[torque_max_status_datagram_size];
+};	
+	
 struct torque_connection_timed_out_event : torque_socket_event
 {
 	torque_connection the_connection;
