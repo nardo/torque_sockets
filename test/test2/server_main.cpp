d6387d6c1b0078cca76a1b4e7d9f56a5fc8768b5
Alex Scarborough <alex@gameclay.com>
2010-01-20 17:32:13 -0800

    Rewrote test2 server - is now server/client and accepts user input.
    This is a much nicer harness for testing client/server connections and has
    the beginnings of introduced connection support (pending backend
    implementation)

--- test/test2/server_main.cpp
+++ test/test2/server_main.cpp
@@ -9,59 +9,131 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <iostream>
 
-bool handle_event(const torque::event& event)
+class harness
 {
-	if (event.event_type() == torque::event::connection_requested_event_type)
+public:
+	harness(const std::string& address) : m_socket(address)
 	{
-//		printf("Connection requested from %s\n", evt.source_address);
-		printf("Connection message: %s\n", event.data().c_str());
-		torque::connection my_connection = event.source_connection();
-		std::string msg = "Hello Mr. Client";
-		my_connection.accept(msg);
-		return false;
+		m_socket.allow_incoming_connections(true);
+		m_socket.can_be_introducer(true);
+		m_socket.set_challenge_response_data("Welcome to the test server.");
 	}
 	
-	if(event.event_type() == torque::event::connection_packet_event_type)
+	void connect(const std::string& address)
 	{
-		printf("Connection data: %s\n", event.data().c_str());
-		return false;
+		m_connections.push_back(m_socket.connect(address, "Connect"));
 	}
 	
-	if (event.event_type() == torque::event::connection_disconnected_event_type)
+	void introduce(const std::string& nonce)
 	{
-		printf("Disconnection message: %s\n", event.data().c_str());
-		return true;
+		m_connections[0].introduce_me(nonce, "Arranged");
 	}
 	
-	if(event.event_type() == torque::event::connection_timed_out_event_type)
+	void do_intro(const std::string& nonce)
 	{
-		printf("Connection timed out\n");
-		return true;
+		m_intro_nonce = nonce;
+	}
+	
+	void run_one_event()
+	{
+		torque::event evt = m_socket.get_next_event();
+		bool end = false;
+		if(evt.event_type())
+		{
+			handle_event(evt);
+		}
 	}
 
-	printf("Unknown event type\n");
-	return false;
-}
+	bool handle_event(const torque::event& event)
+	{
+		if (event.event_type() == torque::event::connection_requested_event_type)
+		{
+	//		printf("Connection requested from %s\n", evt.source_address);
+			printf("Connection message: %s\n", event.data().c_str());
+			m_connections.push_back(event.source_connection());
+			std::string msg = "Hello Mr. Client";
+			m_connections[m_connections.size() - 1].accept(msg);
+			return false;
+		}
+		
+		if(event.event_type() == torque::event::connection_packet_event_type)
+		{
+			printf("Connection data: %s\n", event.data().c_str());
+			return false;
+		}
+		
+		if (event.event_type() == torque::event::connection_disconnected_event_type)
+		{
+			printf("Disconnection message: %s\n", event.data().c_str());
+			return true;
+		}
+		
+		if(event.event_type() == torque::event::connection_timed_out_event_type)
+		{
+			printf("Connection timed out\n");
+			return true;
+		}
+		
+		if(event.event_type() == torque::event::connection_accepted_event_type)
+		{
+			printf("Nonce is: %s\n", event.client_identity().c_str());
+			if(m_connections.size() && m_intro_nonce.length())
+			{
+				introduce(m_intro_nonce);
+				m_intro_nonce = "";
+				return false;
+			}
+		}
+
+		printf("Unknown event type %i\n", (int)event.event_type());
+		return false;
+	}
+	
+private:
+	torque::socket m_socket;
+	std::vector<torque::connection> m_connections;
+	std::string m_intro_nonce;
+};
 
 int main(int argc, const char **argv)
-{	
-	torque::socket my_socket("localhost:31337");
-	my_socket.allow_incoming_connections(true);
-	my_socket.set_challenge_response_data("Welcome to the test server.");
+{
+	std::cout << "Enter address: ";
+	std::string address;
+	std::cin >> address;
+	
+	std::string connect = "";
+	std::cout << "Connect? ";
+	std::cin >> connect;
+	bool do_connect = false;
+	if(connect[0] == 'y')
+	{
+		std::cout << "Connect to: ";
+		std::cin >> connect;
+		do_connect = true;
+	}
+	
+	std::string nonce = "";
+	std::cout << "Introduce me? ";
+	std::cin >> nonce;
+	bool do_intro = false;
+	if(nonce[0] == 'y')
+	{
+		std::cout << "Enter nonce for intro: ";
+		std::cin >> nonce;
+		do_intro = true;
+	}
+	
+	harness h(address);
+	if(do_connect)
+		h.connect(connect);
+	if(do_intro)
+		h.do_intro(nonce);
 
 	while(true)
 	{
-		torque::event evt = my_socket.get_next_event();
-		bool end = false;
-		while(evt.event_type())
-		{
-			if(handle_event(evt))
-				end = true;
-			evt = my_socket.get_next_event();
-		}
-		if(end)
-			break;
+		h.run_one_event();
 		sleep(1);
 	}
 
