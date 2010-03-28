5cd121070469c2dd5ae4cb8f9d6aa06bb73d9cd2
Mark Frohnmayer <mark.frohnmayer@gmail.com>
2010-03-28 11:33:42 -0700

    Part way through nacl build

--- products/libtorquesockets/libtorquesockets_test.c
+++ products/libtorquesockets/libtorquesockets_test.c
@@ -4,28 +4,48 @@
 enum world_type
 {
 	introducer,
-	initiator,
 	host,
+	initiator,
 	introduced_initiator,
 	introduced_host,
+	world_count,
 };
 
-typedef struct world
+enum state
 {
-	int type;
-	torque_socket_handle the_socket;	
-	SOCKADDR_IN bind_address;
-	SOCKADDR_IN connect_address;
+	waiting,
+	connecting_to_remote,
+	connecting_to_introducer,
 };
-	
-int main(int argc, const char **argv)
+
+typedef struct
 {
-	const char *address_string = "127.0.0.1";
-	const char *port_string = "28000";
-	
+	int state;
+	torque_socket_handle _socket;	
+	SOCKADDR_IN connect_address;
+	torque_connection_id connection;
+	torque_connection_id introduced_connection;
+} world;
+
+world g_worlds[world_count];
+int world_ports[world_count] = {
+ 28000, 28001, 0, 0, 0
+};
+
+int get_current_time_ms()
+{
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	int ms = (int) t.tv_sec  * 1000 + t.tv_usec / 1000;
+	return ms;
+}
+
+void init_world(int index)
+{
+	const char *address_string = "127.0.0.1";	
 	int port, host;
 	host = htonl(inet_addr(address_string));
-	port = atoi(port_string);
+	port = world_ports[index];
 	
 	SOCKADDR_IN address;
 	memset(&address, 0, sizeof(address));
@@ -33,8 +53,16 @@ int main(int argc, const char **argv)
 	address.sin_port = htons(port);
 	address.sin_addr.s_addr = htonl(host);
 	
-	torque_socket_handle h;
-	h = torque_socket_create(&address);
+	g_worlds[index] torque_socket_create(&address);	
+}
+
+void process_world(int index)
+{
+	
+}
+	
+int main(int argc, const char **argv)
+{
 	
 	
 }
